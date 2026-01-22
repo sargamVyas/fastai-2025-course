b8a5bf03de9124d4d063c28fc9a43d713a772b4b
Sargam Vyas <sargam.shukla12@gmail.com>
2026-01-21 22:20:22 -0800

    Documentation of initial steps for setting up linear model

--- part1/lesson05/practice/titanice_classification_from_scratch.py
+++ part1/lesson05/practice/titanice_classification_from_scratch.py
@@ -2,19 +2,26 @@
 Data Preparation:
 1. Check for N/A values and replace with mode (works for both category and continious) -> df.mode().iloc[0]
 2. Undesrtand the numeric data -> df.describe()
-3. using df.hist check lon tail dist as some mdoels especially linear dont like it then use log -> np.logp1
+3. using df.hist check long tail dist as some models especially linear dont like it then use log -> np.logp1
 4. Check if numeric col are categorical, 
-5. Convert categorical to dummy as we can mult coeff with string -> pd.get_dummies(df, col_name)
+5. Convert categorical to dummy as we cannot mult coeff with string -> pd.get_dummies(df, col_name)
 6. convert dataframe into tensor (for pytorch) while defining dep and indep column
-Note: tensor.shape ->row * col, len(tensor.shape) -> rank -> vector is rank 1 matrix rank 2
+Note: tensor.shape ->row * col, len(tensor.shape) -> rank -> vector is rank 1, matrix rank 2
 
+Setting up linear model:
+1. Initialize random coeff -> one coeff for each col -> torch.rand(t_indep.shape[1])
+2. Multiply matrix(data) * vector(coeff) using broadcasting because we did element wise multi. So its like vector is broadcasted no of ro times
+3. After multiplication you add the rows together (basic idea of linear model) -> before sum and mult check if any of the col has higher magnitude compared to others(if yes then mormalize the col before mult)
+4. This is out pred -> pred = (t_indep*coeff).sum(axis=1)
+5. Calculate Loss: torch.abs(pred-t_dep).mean()
 
+Gradient descent steps:
+1. Tell pytirch what do you need derivates for -> coeffs.requires_grad_() -> gradient for coeff
 
-
-
-Linear model from scratch steps:
-1. After data cleaning convert data into 
-1. Multiply coefficient with each column
+Notes:
+Try not to use manual_seed as it helps to understand how your data is behaving
+Normalization technique: divide by max , sub by mean and divide by std dev 
+Underscore in dunciotn is in place operation so requires_grad_() will make the change permanently
 '''
 
 import os
